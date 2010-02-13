fcfae3eacf22801bd1cc164742b6868bd476c184
Serge Tarkovski <serge.tarkovski@gmail.com>
2010-02-13 22:14:08 +0200

    ticket:2 CR fixes: added login/logout test

--- src/welcometo42cc/common/tests.py
+++ src/welcometo42cc/common/tests.py
@@ -11,9 +11,37 @@ class WelcomeTo42CcTest(unittest.TestCase):
     def test_homepage(self):
         response = self.client.get('/')
         self.failUnlessEqual(response.status_code, 302)
+        
         self.client.login(username='pioneer', password='123456')
         response = self.client.get('/')
         self.assertTrue("Serge Tarkovski" in response.content)
         self.assertTrue("serge.tarkovski@gmail.com" in response.content)
         self.assertTrue("Cell phone: +380-63-192-4340" in response.content)
         self.assertTrue("Pinocchio is a fictional character that" in response.content)
+    
+    def test_login(self):
+        self.client.logout()
+        
+        response = self.client.get('/login/')
+        self.failUnlessEqual(response.status_code, 200)
+        
+        response = self.client.post('/login/', {'username': 'pioneer', 'password': '123'})
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertTrue("Your username and password didn't match. Please try again." in response.content)
+        
+        response = self.client.post('/login/', {'username': 'abc', 'password': '123456'})
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertTrue("Your username and password didn't match. Please try again." in response.content)
+        
+        response = self.client.post('/login/', {'username': 'pioneer', 'password': '123456'})
+        self.failUnlessEqual(response.status_code, 302)
+        
+        response = self.client.get('/')
+        self.failUnlessEqual(response.status_code, 200)
+        self.failUnlessEqual(response.context['user'].username, 'pioneer')
+        
+        response = self.client.get('/logout/')
+        self.failUnlessEqual(response.status_code, 302)
+        response = self.client.get('/login/')
+        self.failUnlessEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
