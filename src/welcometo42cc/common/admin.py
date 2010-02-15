3188e455587594239c76917bbdb1547dba90199b
Serge Tarkovski <serge.tarkovski@gmail.com>
2010-02-15 19:46:52 +0200

    ticket:3 Implemented middleware, added http log records to admin

--- src/welcometo42cc/common/admin.py
+++ src/welcometo42cc/common/admin.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
+from common.models import HttpRequestLogRecord
+
+
 UserAdmin.fieldsets += (
     ('Extra info', {'fields': ('biography', 'birthdate', 'contacts')}),
     )
+
+class HttpRequestLogRecordAdmin(admin.ModelAdmin):
+    list_display = ('url', 'status_code', 'datetime')
+
+admin.site.register(HttpRequestLogRecord, HttpRequestLogRecordAdmin)
